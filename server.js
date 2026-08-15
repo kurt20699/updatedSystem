bd2c8bb0e7714499690143693e21914e47a0a55f
Kurt Almero <kurtariesr.almero@gmail.com>
2026-08-15 13:38:12 +0800

    fix: increase json body limit for ID photo upload

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ pool.on('connect', (client) => {
 const resetSessions = new Map();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", (_req, res) => {
